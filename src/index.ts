fc8635bd8f5e10e43930682f8fff5378ab0db973
Eron Alves <eron.alves@rocketmail.com>
2026-01-15 00:07:27 -0300

    feat: create a slow web server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import Express from "express";
+import Express, { Request, Response } from "express";
 import { PORT } from "./config";
 import { logger } from "./logger";
 
@@ -7,6 +7,20 @@ const LOG_CONTEXT = "index";
 const LOG = logger(LOG_CONTEXT);
 
 const app = Express();
+
+const MOCK_PLANET_NAME = "Tatooine";
+const MOCK_PLANET_CLIMATE = "Arid";
+const GET_PLANET_BY_ID_ROUTE = "/planets/:id";
+
+app.get(GET_PLANET_BY_ID_ROUTE, (req: Request, res: Response) => {
+  setTimeout(() => {
+    res.send({
+      id: req.params.id,
+      name: MOCK_PLANET_NAME,
+      climate: MOCK_PLANET_CLIMATE,
+    });
+  }, 3000);
+});
 
 app.listen(PORT, (err) => {
   if (!err) {
