111ee250a6cc1e5c68d7aad07843b26412de1076
Eron Alves <eron.alves@rocketmail.com>
2026-01-22 23:23:47 -0300

    feat: create a dotenv file

--- src/redis.ts
+++ src/redis.ts
@@ -1,10 +1,18 @@
 import { createClient } from "redis";
 import { REDIS_URL } from "./config";
+import { logger } from "./logger";
 
 export const client = createClient({ url: REDIS_URL });
 
+const LOGGER_META = "redis";
+const LOG = logger(LOGGER_META);
+
 client.on("error", (err) => {
   throw new Error(err);
+});
+
+client.on("ready", () => {
+  LOG.info("Redis client connected");
 });
 
 client.connect();
