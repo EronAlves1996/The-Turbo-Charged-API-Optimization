95dbbfdbbacb13bd142bc7bd735869faa3ed9caa
Eron Alves <eron.alves@rocketmail.com>
2026-02-24 15:25:16 -0300

    feat: configure cluster

--- src/config.ts
+++ src/config.ts
@@ -12,8 +12,16 @@ const redisSentinelNodesUnparsed = process.env.REDIS_SENTINEL_NODES_URL || "";
 if (!redisSentinelNodesUnparsed) {
   throw new Error(REDIS_SENTINEL_NODES_URL_NOT_DEFINED_MESSAGE);
 }
+const redisClusterNodesUnparsed = process.env.REDIS_CLUSTER_NODES_URL;
+const REDIS_CLUSTER_NODES_URL_NOT_DEFINED_MESSAGE =
+  "Required parameter REDIS_CLUSTER_NODES_URL not defined";
+
+if (!redisClusterNodesUnparsed) {
+  throw new Error(REDIS_CLUSTER_NODES_URL_NOT_DEFINED_MESSAGE);
+}
 
 export const REDIS_SENTINEL_NODES_URL = redisSentinelNodesUnparsed.split(";");
+export const REDIS_CLUSTER_NODES_URL = redisClusterNodesUnparsed.split(";");
 
 export const REDIS_MASTER_NAME = process.env.REDIS_MASTER_NAME || "";
 export const REDIS_NODES_HOST = process.env.REDIS_NODES_HOST || "";
@@ -26,5 +34,6 @@ if (LOG_LEVEL === "debug") {
     REDIS_SENTINEL_NODES_URL,
     REDIS_MASTER_NAME,
     REDIS_NODES_HOST,
+    REDIS_CLUSTER_NODES_URL,
   });
 }
--- src/redis.ts
+++ src/redis.ts
@@ -1,40 +1,17 @@
-import { createSentinel } from "redis";
+import { createCluster } from "redis";
 import { logger } from "./logger";
-import { REDIS_MASTER_NAME, REDIS_SENTINEL_NODES_URL } from "./config";
+import { REDIS_CLUSTER_NODES_URL } from "./config";
 
 const LOGGER_META = "redis";
 const LOG = logger(LOGGER_META);
 
 async function createRedisClient() {
-  const resolvedRootNodes = REDIS_SENTINEL_NODES_URL.map((n) => {
-    const [host, port] = n.split(":");
-    return {
-      host: host ?? "",
-      port: Number(port ?? ""),
-    };
+  const client = createCluster({
+    rootNodes: REDIS_CLUSTER_NODES_URL.map((url) => ({ url })),
   });
-
-  LOG.debug("Redis sentinel resolved root nodes", { resolvedRootNodes });
-
-  const client = createSentinel({
-    name: REDIS_MASTER_NAME,
-    sentinelRootNodes: resolvedRootNodes,
-    nodeClientOptions: {
-      disableOfflineQueue: true,
-      socket: {
-        reconnectStrategy: 5000,
-        timeout: 5000,
-        connectTimeout: 5000,
-        sessionTimeout: 5000,
-      },
-    },
-    masterPoolSize: 2,
-    replicaPoolSize: 2,
-  });
-
-  client.setTracer = console.log;
 
   LOG.debug("Initializing connection to redis");
+
   await client
     .on("error", (err) => {
       LOG.error("redis client error", err);
